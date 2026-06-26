e9d82936517823149ccd699d44c6c815ad8bff7b
Mahendra Kamejaliya <mahendra.kamejaliya07@gmail.com>
2026-06-26 22:27:59 +0530

    Time: 3 ms (29.04%) | Memory: 9.8 MB (94.30%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isAN(char ch) {
+        if ((ch >= '0' && ch <= '9') ||
+            (tolower(ch) >= 'a' && tolower(ch) <= 'z')) {
+            return true;
+        }
+        return false;
+    }
+
+    bool isPalindrome(string s) {
+        int st = 0, end = s.length() - 1;
+
+        while (st < end) {
+
+            if (!isAN(s[st])) {
+                st++;
+                continue;
+            }
+
+            if (!isAN(s[end])) {
+                end--;
+                continue;
+            }
+
+            if (tolower(s[st]) != tolower(s[end])) {
+                return false;
+            }
+
+            st++;
+            end--;
+        }
+
+        return true;
+    }
+};
